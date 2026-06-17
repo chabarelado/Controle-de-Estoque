7c3338c95c5db9364a6c5da9a7bc394e0df135e8
Chabarelado <josmachado48@gmail.com>
2026-06-17 16:11:12 -0300

    12 Commit | Iniciação a criação de metodos do Repositório de Movimentos

--- backend/src/repositories/repositorio_movimento.py
+++ backend/src/repositories/repositorio_movimento.py
@@ -1,18 +1,73 @@
-
+from api.api import Api
+from repositories.repositorio_destinatario import RepositorioDestinatario
+from repositories.repositorio_peca import RepositorioPecas
 
 class RepositorioMovimentacao:
+    def __init__(self):
+        self.api = Api()
 
-    def registrar_movimento(self):
-        pass
+    def adicionar(self, novo_movimento):
+        if not self.api.ler_json():
+            return False
+        
+        dados = self.api.dados
+        novo_movimento["id"] = self.proximo_id(dados)
+
+        try:
+            dados["movimentos"].append(novo_movimento)
+            return self.api.salvar_json()
+        except Exception as e:
+            print("Erro ao registrar movimento.")
+            return False
+    
+    def proximo_id(self, dados=None):
+        if dados is None:
+            if not self.api.ler_json():
+                return False
+            
+            dados = self.api.dados
+
+        movimentos = dados["movimentos"]
+
+        if not movimentos:
+            return False
+
+        return max(movi["id"] for movi in movimentos) + 1
 
     def listar(self):
-        pass
+        if not self.api.ler_json():
+            return []
+        
+        return self.api.dados["movimentos"]
 
     def desativar(self, id):
         pass
 
-    def atualizar(self, id):
+    def cancelar_movimento(self, dados=None):
+        if dados is None:
+            if not self.api.ler_json():
+                return False
+
+            dados = self.api.dados
+
+        verifica_mov = self.buscar_movimento(id, dados)
         pass
 
-    def buscar_por_nome(self, nome, dados=None):
+    def buscar_por_id(self,id, dados=None):
+        if dados is None:
+            if not self.api.ler_json():
+                return None
+            dados = self.api.dados
+        
+        for movimento in dados["movimentos"]:
+            if id == movimento["id"]:
+                return movimento
+        
+        return None
+
+    def buscar_por_peca(self, nome=None, id=None, dados=None):
         pass
+    
+    def buscar_por_destinatario(self, nome=None, id=None, dados=None):
+        pass
+
