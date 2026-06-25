15b2edc98ce98933b69fd878aa1d7395c9288fb5
Chabarelado <josmachado48@gmail.com>
2026-06-25 13:04:06 -0300

    18 Commit | Atualizando o Service

--- backend/src/services/estoque_service.py
+++ backend/src/services/estoque_service.py
@@ -2,11 +2,20 @@ from repositories.repositorio_movimento import RepositorioMovimentacao
 from repositories.repositorio_peca import RepositorioPecas
 from repositories.repositorio_destinatario import RepositorioDestinatario
 
+from models.pecas import Peca
+from models.destinatario import Destinatario
+from models.movimento import Movimentacao
+
 class Service:
     def __init__(self):
-        self.movimento = RepositorioMovimentacao()
-        self.peca = RepositorioPecas()
-        self.destinatario = RepositorioDestinatario()
+        self.repo_movi = RepositorioMovimentacao()
+        self.repo_peca = RepositorioPecas()
+        self.repo_unid = RepositorioDestinatario()
+
+        self.peca = Peca()
+        self.unidade = Destinatario()
+        self.movimento = Movimentacao()
+        
 
     def registar_entrada(self):
         pass
